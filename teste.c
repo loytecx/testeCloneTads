f8873078dbf7248fff76d1101876558955ac9a15
Jefferson Rosa <loytecx@yahoo.com.br>
2020-02-14 10:16:35 -0300

    nova ver

--- teste.c
+++ teste.c
@@ -3,6 +3,17 @@
 
 int main(){
 
+	int num;
 
+	printf("Digite um número: \n");
+	scanf("%i", &num);
+	
+	if (num % 2 == 0){
+		printf("É par...\n");
+	}
+	else{
+		printf("É impar...\n");
+	}
+	
     return 0;
 }
